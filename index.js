33df15e54a742d0f63f8375efecb2ce25a0e9eec
MauricioMiramontesTelevisatim <mauricio.miramontes@televisatim.com>
2022-03-29 17:35:35 -0600

    chore: ✅  archivos de configuracion y dependencias de desarrollo
    Se agrega prettier, husky, lintedstaged, commitlint
    
    Co-authored-by: CariJG <CariJG@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,56 +1,57 @@
-const findUp = require('find-up')
-const fs = require('fs')
-const homedir = require('homedir')
-const truncate = require('cli-truncate')
-const wrap = require('wrap-ansi')
-const pad = require('pad')
-const path = require('path')
-const fuse = require('fuse.js')
-const util = require('util')
+/* eslint-disable prettier/prettier */
+const findUp = require("find-up");
+const fs = require("fs");
+const homedir = require("homedir");
+const truncate = require("cli-truncate");
+const wrap = require("wrap-ansi");
+const pad = require("pad");
+const path = require("path");
+const Fuse = require("fuse.js");
+const util = require("util");
 
-const readFile = util.promisify(fs.readFile)
+const readFile = util.promisify(fs.readFile);
 
-const types = require('./lib/types')
-const SCOPES = require('./lib/scopes')
+const TYPES = require("./lib/types.json");
+const SCOPES = require("./lib/scopes.json");
 
 
 function loadConfig(filename) {
-  return readFile(filename, 'utf8')
+  return readFile(filename, "utf8")
     .then(JSON.parse)
-    .then(obj => obj && obj.config && obj.config['cz-configuracion-es'])
-    .catch(() => null)
+    .then(obj => obj && obj.config && obj.config["cz-configuracion-es"])
+    .catch(() => null);
 }
 
 function loadConfigUpwards(filename) {
-  return findUp(filename).then(loadConfig)
+  return findUp(filename).then(loadConfig);
 }
 
 async function getConfig() {
   const defaultConfig = {
-    types,
+    types: TYPES,
     symbol: true,
-    skipQuestions: [''],
+    skipQuestions: [""],
     subjectMaxLength: 75,
-  }
+  };
 
   const config =
-    (await loadConfigUpwards('package.json')) ||
-    (await loadConfigUpwards('.czrc')) ||
-    (await loadConfig(path.join(homedir(), '.czrc')))
+    (await loadConfigUpwards("package.json")) ||
+    (await loadConfigUpwards(".czrc")) ||
+    (await loadConfig(path.join(homedir(), ".czrc")));
 
-  return { ...defaultConfig, ...config }
+  return { ...defaultConfig, ...config };
 }
 
 function reducerNameLength(array) {
   const maxNameLength = array.reduce(
     (maxLength, element) => (element.name.length > maxLength ? element.name.length : maxLength),
     0
-  )
-  return maxNameLength
+  );
+  return maxNameLength;
 }
 
 function getEmojiChoices({ types, symbol }) {
-  const maxLengthName = reducerNameLength(types)
+  const maxLengthName = reducerNameLength(types);
   return types.map(choice => ({
     name: `${pad(choice.name, maxLengthName)}  ${choice.emoji}  ${choice.description}`,
     value: {
@@ -58,45 +59,45 @@ function getEmojiChoices({ types, symbol }) {
       name: choice.name
     },
     code: choice.code
-  }))
+  }));
 }
 
-function getScopeChoices( SCOPES ) {
-  const maxLengthName = reducerNameLength(SCOPES)
-  return SCOPES.map(scope => ({
-      name: `${pad(scope.name, maxLengthName)}  ${scope.description}`,
-      value: scope.value
-    })
-  )
+function getScopeChoices( scopes ) {
+  const maxLengthName = reducerNameLength(scopes);
+  return scopes.map(scope => ({
+    name: `${pad(scope.name, maxLengthName)}  ${scope.description}`,
+    value: scope.value
+  }));
 }
 
 function formatScope(scope, secondScope) {
-  return scope !== ' ' ? `(${scope}${secondScope !== ' ' ? `, ${secondScope}` : ''})` : ''
+  return scope !== " " ? `(${scope}${secondScope !== " " ? `, ${secondScope}` : ""})` : "";
 }
 
 function formatHead({ type, scope, secondScope, subject }) {
-  const prelude = `${type.name}${formatScope(scope, secondScope)}: ${type.emoji}`
-  return `${prelude} ${subject.toLowerCase()}`
+  const prelude = `${type.name}${formatScope(scope, secondScope)}: ${type.emoji}`;
+  return `${prelude} ${subject.toLowerCase()}`;
 }
 
 function formatIssues(issues) {
-  return issues ? 'Closes ' + (issues.match(/#\w+/g) || []).join(', closes ') + '\n' : ''
+  return issues ? `Closes ${  (issues.match(/#\w+/g) || []).join(", closes ")  }\n` : "";
 }
 
 function formatCoAuthor(coAuthor) {
-  let string_coAuthor = coAuthor ? coAuthor.replace(' ', '') : '';
-  let author_array = string_coAuthor.split(',');
-  let result_string = '';
-  for (const AUTHOR in author_array) {
-      if(string_coAuthor){
-        result_string += `Co-authored-by: ${author_array[AUTHOR]} <${author_array[AUTHOR]}@users.noreply.github.com>${author_array.length === 1 || AUTHOR == author_array.length-1 ?'':'\n'}`
+  const STRING_COAUTHOR = coAuthor ? coAuthor.replace(" ", "") : "";
+  const AUTHOR_ARRAY = STRING_COAUTHOR.split(",");
+  let resultString = "";
+  // eslint-disable-next-line no-restricted-syntax
+  for (const AUTHOR in AUTHOR_ARRAY) {
+      if(STRING_COAUTHOR){
+        resultString += `Co-authored-by: ${AUTHOR_ARRAY[AUTHOR]} <${AUTHOR_ARRAY[AUTHOR]}@users.noreply.github.com>${AUTHOR_ARRAY.length === 1 || AUTHOR === AUTHOR_ARRAY.length-1 ?"":"\n"}`;
       }
   }
-  return result_string
+  return resultString;
 }
 
 function secondListScopes (scope, firstListScopes){
-  let newListScope =  firstListScopes.filter((element) => element.value !== scope.scope);
+  const newListScope =  firstListScopes.filter((element) => element.value !== scope.scope);
   return newListScope;
 }
 
@@ -110,108 +111,108 @@ function secondListScopes (scope, firstListScopes){
  * @private
  */
 function createQuestions(config) {
-  const choices = getEmojiChoices(config)
-  const scopes = getScopeChoices(SCOPES)
+  const choices = getEmojiChoices(config);
+  const scopes = getScopeChoices(SCOPES);
 
-  const fuzzyTypes = new fuse(choices, {
+  const fuzzyTypes = new Fuse(choices, {
     shouldSort: true,
     threshold: 0.4,
     location: 0,
     distance: 100,
     maxPatternLength: 32,
     minMatchCharLength: 1,
-    keys: ['name', 'code']
-  })
+    keys: ["name", "code"]
+  });
 
-  const fuzzyScopes = new fuse(scopes, {
+  const fuzzyScopes = new Fuse(scopes, {
     shouldSort: true,
     threshold: 0.4,
     location: 0,
     distance: 100,
     maxPatternLength: 32,
     minMatchCharLength: 1,
-    keys: ['name', 'code']
-  })
+    keys: ["name", "code"]
+  });
 
   const questions = [
     {
-      type: 'autocomplete',
-      name: 'type',
+      type: "autocomplete",
+      name: "type",
       message:
         config.questions && config.questions.type
           ? config.questions.type
-          : 'Selecciona el tipo del commit:\n',
+          : "Selecciona el tipo del commit:\n",
       source: (_, query) => Promise.resolve(query ? fuzzyTypes.search(query) : choices)
     },
     {
-      type: 'autocomplete',
-      name: 'scope',
+      type: "autocomplete",
+      name: "scope",
       message:
         config.questions && config.questions.scope
           ? config.questions.scope
-          : '¿Cuál es el primer scope (archivo o tag)? (opcional):\n',
+          : "¿Cuál es el primer scope (archivo o tag)? (opcional):\n",
       source: (_, query) => Promise.resolve(query ? fuzzyScopes.search(query) : scopes),
 
     },
     {
-      type: 'autocomplete',
-      name: 'secondScope',
+      type: "autocomplete",
+      name: "secondScope",
       message:
         config.questions && config.questions.scope
           ? config.questions.scope
-          : '¿Cuál es el segundo scope (archivo o tag)? (opcional):\n',
+          : "¿Cuál es el segundo scope (archivo o tag)? (opcional):\n",
       source: (_, query) => Promise.resolve(query ? fuzzyScopes.search(query) : secondListScopes(_, scopes)),
-      when: answers => answers.scope !== ' ',
+      when: answers => answers.scope !== " ",
     },
     {
-      type: 'maxlength-input',
-      name: 'subject',
+      type: "maxlength-input",
+      name: "subject",
       message:
         config.questions && config.questions.subject
           ? config.questions.subject
-          : 'Título descriptivo corto:\n',
+          : "Título descriptivo corto:\n",
       maxLength: config.subjectMaxLength-1,
       filter: (subject, answers) => formatHead({ ...answers, subject })
     },
     {
-      type: 'input',
-      name: 'body',
+      type: "input",
+      name: "body",
       message:
         config.questions && config.questions.body
           ? config.questions.body
-          : 'Descripción detallada del cambio (opcional):\n',
-      when: !config.skipQuestions.includes('body'),
+          : "Descripción detallada del cambio (opcional):\n",
+      when: !config.skipQuestions.includes("body"),
     },
     {
-      type: 'input',
-      name: 'breakingBody',
+      type: "input",
+      name: "breakingBody",
       message:
         config.questions && config.questions.breaking
           ? config.questions.breaking
-          : '¿Estos cambios rompen alguna parte del sistema? Descripcion corta (opcional):\n',
-      when: !config.skipQuestions.includes('breaking')
+          : "¿Estos cambios rompen alguna parte del sistema? Descripcion corta (opcional):\n",
+      when: !config.skipQuestions.includes("breaking")
     },
     {
-      type: 'input',
-      name: 'coAuthor',
+      type: "input",
+      name: "coAuthor",
       message:
         config.questions && config.questions.coauthored
           ? config.questions.coauthored
-          : 'Nombre de usuario de github de co-autor(es) (ej: Usuario1, Usuario2, ...) (opcional):\n',
-      when: !config.skipQuestions.includes('coauthored')
+          : "Nombre de usuario de github de co-autor(es) (ej: Usuario1, Usuario2, ...) (opcional):\n",
+      when: !config.skipQuestions.includes("coauthored")
     },
     {
-      type: 'input',
-      name: 'issues',
+      type: "input",
+      name: "issues",
       message:
         config.questions && config.questions.issues
           ? config.questions.issues
-          : 'Issues relacionados (ej:#1, #2, ...) (opcional):\n',
-      when: !config.skipQuestions.includes('issues')
+          : "Issues relacionados (ej:#1, #2, ...) (opcional):\n",
+      when: !config.skipQuestions.includes("issues")
     }
-  ]
+  ];
 
-  return questions
+  return questions;
 }
 
 /**
@@ -221,21 +222,21 @@ function createQuestions(config) {
  * @return {String} Formated git commit message
  */
 function format(answers) {
-  const { columns } = process.stdout
+  const { columns } = process.stdout;
 
-  const head = truncate(answers.subject, columns)
-  const body = wrap(answers.body || '', columns)
+  const head = truncate(answers.subject, columns);
+  const body = wrap(answers.body || "", columns);
   const breaking =
     answers.breakingBody && answers.breakingBody.trim().length !== 0
       ? wrap(`BREAKING CHANGE: ${answers.breakingBody.trim()}`, columns)
-      : ''
+      : "";
 
-  const footer = formatIssues(answers.issues) + formatCoAuthor(answers.coAuthor)
+  const footer = formatIssues(answers.issues) + formatCoAuthor(answers.coAuthor);
 
   return [head, body, breaking, footer]
     .filter(Boolean)
-    .join('\n\n')
-    .trim()
+    .join("\n\n")
+    .trim();
 }
 
 /**
@@ -244,14 +245,16 @@ function format(answers) {
  * @type {Object}
  */
 module.exports = {
-  prompter: function(cz, commit) {
-    cz.prompt.registerPrompt('autocomplete', require('inquirer-autocomplete-prompt'))
-    cz.prompt.registerPrompt('maxlength-input', require('inquirer-maxlength-input-prompt'))
+  prompter(cz, commit) {
+    // eslint-disable-next-line global-require
+    cz.prompt.registerPrompt("autocomplete", require("inquirer-autocomplete-prompt"));
+    // eslint-disable-next-line global-require
+    cz.prompt.registerPrompt("maxlength-input", require("inquirer-maxlength-input-prompt"));
 
     getConfig()
       .then(createQuestions)
       .then(cz.prompt)
       .then(format)
-      .then(commit)
+      .then(commit);
   }
-}
+};
